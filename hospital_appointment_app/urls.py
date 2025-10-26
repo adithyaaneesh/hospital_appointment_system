94971ce3c5506161f8fedda9134fc07e6bbe39e2
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-10-26 07:05:57 +0530

    add doctors & book_appointment

--- hospital_appointment_app/urls.py
+++ hospital_appointment_app/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book_appointment', views.book_appointment, name='book_appointment'),
+    path('doctors', views.doctor, name='doctors'),
 ]
--- hospital_appointment_app/views.py
+++ hospital_appointment_app/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Appointment, Doctor
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def doctor(request):
+    if request.method == 'POST':
+        Doctor.objects.create(
+            name=request.POST.get('name'),
+            specialty=request.POST.get('specialty'),
+            available_time=request.POST.get('available_time')
+        )
+        return redirect('index')
+    return render(request, 'doctors.html')
+
+
+def book_appointment(request):
+    doctors = Doctor.objects.all()
+    if request.method == 'POST':
+        doctor_id = request.POST.get('doctor')
+        doctor = Doctor.objects.get(id=doctor_id)
+
+        Appointment.objects.create(
+            patient_name=request.POST.get('patient_name'),
+            doctor=doctor,
+            date=request.POST.get('date'),
+            time_slot=request.POST.get('time_slot'),
+            status='pending',
+        )
+        return redirect('index')
+    return render(request, 'book_appointment.html', {'doctors': doctors})
